0841599572aa8b3392f42ba6ec6baa4aa460c768
Axel Simms <mail@axelsimms.com>
2020-07-19 00:26:06 -0400

    added integration tests and seperated out some of the librarires

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ https://os.phil-opp.com/
 ## Steps in creating a Rust OS
 1. Create a freestandung Rust Binary [done]
 2. Write a microkernel (x86) [done]
-3. VGA text mode [in-progress]
-4. Add unit and integration testing
+3. VGA text mode [done]
+4. Add unit and integration testing [in progress]
 5. Catch CPU Exceptions
 6. Avoid triple faults
 7. Forward hardware interrupts to the CPU
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 #![no_std] // unlink the rust standard library
 #![no_main] // disable all rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-extern crate rlibc;
-
+use blog_os::println;
 use core::panic::PanicInfo;
-
-mod vga_buffer;
 
 // Instead of a main function (needs runtime), we write our own start function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("This is dragon's operating system{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-// This function is called on panic. Must be reimplemented since we are not using std
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
