135566f9c05454111def030ca4f296eed881098f
Axel Simms <mail@axelsimms.com>
2020-07-18 23:27:48 -0400

    added a function that writes strings to the vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
 #![no_std] // unlink the rust standard library
 #![no_main] // disable all rust-level entry points
+
 extern crate rlibc;
 
 use core::panic::PanicInfo;
 
-// This function is called on panic. Must be reimplemented since we are not using std
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 // Instead of a main function (needs runtime), we write our own start function
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("This is dragon's operating system{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    loop {}
+}
 
+// This function is called on panic. Must be reimplemented since we are not using std
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
